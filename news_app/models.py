999dd6fbce3191629e6248ad7af46bbf5a75472f
Kiddo-prog <husseinmubarak500@gmail.com>
2022-05-15 13:11:04 +0100

    Increased VARCHAR in models.py

--- news_app/models.py
+++ news_app/models.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 
 class Article(models.Model):
     name = models.CharField(max_length=100, default='NY Times')
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=400)
     url = models.URLField()
     description = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
     image = models.URLField()
-    guid = models.CharField(max_length=50)
+    guid = models.CharField(max_length=400)
 
     def __str__(self):
         return f"{self.title}"
